046c0cb9c2add1049fe48cd23f869ef602cdcaa1
Olya <o.us@sw-tech.by>
2021-07-16 15:07:45 +0300

    Second page

--- js/index.js
+++ js/index.js
@@ -110,13 +110,43 @@ $(document).ready(function () {
     });
     $('.form-controller').blur();
 
-    $('.courses-cards').slick({
+    $('#courses-cards').slick({
         infinite: false,
         slidesToShow: 6,
         slidesToScroll: 1,
         autoplaySpeed: 3000,
         dots: false,
-        arrows: true
+        arrows: true,
+        responsive: [
+          {
+            breakpoint: 1400,
+            settings: {
+              slidesToShow: 4,
+              slidesToScroll: 1
+            }
+          },
+          {
+            breakpoint: 1200,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 1
+            }
+          },
+          {
+            breakpoint: 992,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 1
+            }
+          },
+          {
+            breakpoint: 576,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+        ]
     });
 });
 
