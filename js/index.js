9c479d5bc76bcf515e956864decf18fcebb641d9
Olya Us <olyaus0809@gmail.com>
2021-08-15 22:27:36 +0300

    Course page

--- js/index.js
+++ js/index.js
@@ -234,5 +234,43 @@ $(document).ready(function () {
           }
         ]
     });
+        $('#courses-cards-camp').slick({
+        infinite: false,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplaySpeed: 3000,
+        dots: false,
+        arrows: true,
+        responsive: [
+          {
+            breakpoint: 1400,
+            settings: {
+              slidesToShow: 4,
+              slidesToScroll: 1
+            }
+          },
+          {
+            breakpoint: 1200,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 1
+            }
+          },
+          {
+            breakpoint: 992,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 1
+            }
+          },
+          {
+            breakpoint: 576,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+        ]
+    });
 });
 
